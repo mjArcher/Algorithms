d7cee45c7b39dc2be34c865c69e88b2860d3e3ea
Matthew Archer <matt.james.archer@gmail.com>
2014-01-13 14:48:58 +0000

    Setup

---
+++ simple.cpp
@@ -0,0 +1,3 @@
+//this is a simple document which comprises a selection of implemented algorithms
+//from Cormen, Leiserson, Rivest and Stein. It will serve as a reference.
+//It is also being developed to understand how git works. 
