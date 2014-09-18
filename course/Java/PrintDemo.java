c700304f83fdf6458d77023d85996d329dd09f36
Matthew Archer <matt.james.archer@gmail.com>
2014-09-18 22:22:51 +0100

    changed dir structure and coded first week

--- course/Java/PrintDemo.java
+++
@@ -1,8 +0,0 @@
-public class PrintDemo{
-
-	public static void main(String[] args){
-		Print pr1 = new Print();
-		Print pr2 = new Print();
-		pr1.printNumber();
-	}
-}
--- f_code/Insertion.f90
+++
@@ -1,6 +0,0 @@
-
-program HelloWorld
-  write (*,*) 'Hello, world!'   ! This is an inline comment
-end program HelloWorld
-
-
