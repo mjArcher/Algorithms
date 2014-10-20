8823a24651e3fc2f359d2786b6b1209b1d503ddd
Matthew Archer <matt.james.archer@gmail.com>
2014-10-20 11:36:27 +0100

    debugging

--- course/alg_code/Week2/Test.java
+++ course/alg_code/Week2/Test.java
@@ -13,7 +13,6 @@ class Test {
     Stack<Integer> stack = new Stack<Integer>(); // in comments below we have some behind the scenes casting
     s.push(17); //s.push(new Integer(17));
     int a = s.pop();//int a = s.pop().intValue();
-
     //bottom line is that client code can use generic stack for any type of data
   }
 }
